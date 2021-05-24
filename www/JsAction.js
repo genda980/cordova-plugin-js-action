169afdb75ed108c0eece8a447c5c167cda02b5ee
lzd <jiusanlzd@163.com>
2021-05-24 15:53:31 +0800

    first

--- www/JsAction.js
+++ www/JsAction.js
@@ -1,5 +1,27 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'JsAction', 'coolMethod', [arg0]);
+exports.open = function (arg0, success, error) {
+    exec(success, error, 'JsAction', 'open', [arg0]);
+};
+
+exports.close = function (arg0, success, error) {
+    exec(success, error, 'JsAction', 'close', [arg0]);
+};
+
+exports.action = function (arg0, success, error) {
+    exec(success, error, 'JsAction', 'action', [arg0]);
+};
+
+exports.onAction = function (eventId, params) {
+    cordova.fireDocumentEvent('JsAction.onAction', {
+        eventId: eventId,
+        params: params
+    });
+};
+
+exports.onEventDocument = function (eventId, params) {
+    cordova.fireDocumentEvent('JsAction.onEventDocument', {
+        eventId: eventId,
+        params: params
+    });
 };
